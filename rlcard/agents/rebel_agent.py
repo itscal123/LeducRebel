05758d5c4cb36c00e25330fbd39307c7191bac4e
itscal123 <calvin.kory@gmail.com>
2022-05-08 18:32:16 -0700

    Changes to rebel_agent.py

--- rlcard/agents/rebel_agent.py
+++ rlcard/agents/rebel_agent.py
@@ -3,6 +3,8 @@ import collections
 
 import os
 import pickle
+import torch
+import torch.nn as nn
 
 from rlcard.utils.utils import *
 
@@ -42,6 +44,34 @@ class RebelAgent():
 
         # Update policy
         self.update_policy()
+
+
+    # TODO: Implement Rebel Logic from paper
+    def train2(self):
+        """
+        Rebel algorithm for RL and Search for Imperfect-Information Games
+        Rough pseudocode:
+        while !ISTERMINAL(βr) do
+            G ← CONSTRUCTSUBGAME(βr)
+            π_bar, π_t_warm ← INITIALIZEPOLICY(G, θπ)
+            G ← SETLEAFVALUES(G, π _bar, π_t_warm , θv)
+            v(βr) ← COMPUTEEV(G, π_t_warm )
+            tsample ∼ unif {t_warm + 1, T }
+            for t = (t_warm + 1)..T do
+                if t = tsample then
+                    βr′ ←SAMPLELEAF(G,π_t − 1)
+                π_t UPDATEPOLICY(G, π_t − 1)
+                π_bar ← t π _bar + 1 π t
+                G ← SETLEAFVALUES(G, π_bar, π_t, θv)
+                v(βr)← t v(βr)+ 1 COMPUTEEV(G,π_t) t+1 t+1
+            Add {βr , v(βr )} to D_v 
+            for β ∈ G do
+                Add {β, π_bar(β)} to D_π 
+            βr ← βr′
+        """
+        while not self.env.is_over():
+            subgame = constructSubGame  # TODO
+
 
     def traverse_tree(self, probs, player_id):
         ''' Traverse the game tree, update the regrets
@@ -102,6 +132,7 @@ class RebelAgent():
         '''
         for obs in self.regrets:
             self.policy[obs] = self.regret_matching(obs)
+
 
     def regret_matching(self, obs):
         ''' Apply regret matching
@@ -219,3 +250,178 @@ class RebelAgent():
         self.iteration = pickle.load(iteration_file)
         iteration_file.close()
 
+class Estimator(object):
+    '''
+    Approximate clone of rlcard.agents.dqn_agent.Estimator that
+    uses PyTorch instead of Tensorflow.  All methods input/output np.ndarray.
+
+    Estimator neural network.
+    This network is used for both the value network and the policy network.
+    '''
+
+    def __init__(self, num_actions=2, learning_rate=0.001, state_shape=None, mlp_layers=None, device=None):
+        ''' Initilalize an Estimator object.
+
+        Args:
+            num_actions (int): the number output actions
+            state_shape (list): the shape of the state space
+            mlp_layers (list): size of outputs of mlp layers
+            device (torch.device): whether to use cpu or gpu
+        '''
+        self.num_actions = num_actions
+        self.learning_rate=learning_rate
+        self.state_shape = state_shape
+        self.mlp_layers = mlp_layers
+        self.device = device
+
+        # set up Q model and place it in eval mode
+        qnet = EstimatorNetwork(num_actions, state_shape, mlp_layers)
+        qnet = qnet.to(self.device)
+        self.qnet = qnet
+        self.qnet.eval()
+
+        # initialize the weights using Xavier init
+        for p in self.qnet.parameters():
+            if len(p.data.shape) > 1:
+                nn.init.xavier_uniform_(p.data)
+
+        # set up loss function
+        self.mse_loss = nn.MSELoss(reduction='mean')
+
+        # set up optimizer
+        self.optimizer =  torch.optim.Adam(self.qnet.parameters(), lr=self.learning_rate)
+
+    def predict_nograd(self, s):
+        ''' Predicts action values, but prediction is not included
+            in the computation graph.  It is used to predict optimal next
+            actions in the Double-DQN algorithm.
+
+        Args:
+          s (np.ndarray): (batch, state_len)
+
+        Returns:
+          np.ndarray of shape (batch_size, NUM_VALID_ACTIONS) containing the estimated
+          action values.
+        '''
+        with torch.no_grad():
+            s = torch.from_numpy(s).float().to(self.device)
+            q_as = self.qnet(s).cpu().numpy()
+        return q_as
+
+    def update(self, s, a, y):
+        ''' Updates the estimator towards the given targets.
+            In this case y is the target-network estimated
+            value of the Q-network optimal actions, which
+            is labeled y in Algorithm 1 of Minh et al. (2015)
+
+        Args:
+          s (np.ndarray): (batch, state_shape) state representation
+          a (np.ndarray): (batch,) integer sampled actions
+          y (np.ndarray): (batch,) value of optimal actions according to Q-target
+
+        Returns:
+          The calculated loss on the batch.
+        '''
+        self.optimizer.zero_grad()
+
+        self.qnet.train()
+
+        s = torch.from_numpy(s).float().to(self.device)
+        a = torch.from_numpy(a).long().to(self.device)
+        y = torch.from_numpy(y).float().to(self.device)
+
+        # (batch, state_shape) -> (batch, num_actions)
+        q_as = self.qnet(s)
+
+        # (batch, num_actions) -> (batch, )
+        Q = torch.gather(q_as, dim=-1, index=a.unsqueeze(-1)).squeeze(-1)
+
+        # update model
+        batch_loss = self.mse_loss(Q, y)
+        batch_loss.backward()
+        self.optimizer.step()
+        batch_loss = batch_loss.item()
+
+        self.qnet.eval()
+
+        return batch_loss
+
+
+class EstimatorNetwork(nn.Module):
+    ''' The function approximation network for Estimator
+        It is just a series of tanh layers. All in/out are torch.tensor
+    '''
+
+    def __init__(self, num_actions=2, state_shape=None, mlp_layers=None):
+        ''' Initialize the Q network
+
+        Args:
+            num_actions (int): number of legal actions
+            state_shape (list): shape of state tensor
+            mlp_layers (list): output size of each fc layer
+        '''
+        super(EstimatorNetwork, self).__init__()
+
+        self.num_actions = num_actions
+        self.state_shape = state_shape
+        self.mlp_layers = mlp_layers
+
+        # build the Q network
+        layer_dims = [np.prod(self.state_shape)] + self.mlp_layers
+        fc = [nn.Flatten()]
+        fc.append(nn.BatchNorm1d(layer_dims[0]))
+        for i in range(len(layer_dims)-1):
+            fc.append(nn.Linear(layer_dims[i], layer_dims[i+1], bias=True))
+            fc.append(nn.Tanh())
+        fc.append(nn.Linear(layer_dims[-1], self.num_actions, bias=True))
+        self.fc_layers = nn.Sequential(*fc)
+
+    def forward(self, s):
+        ''' Predict action values
+
+        Args:
+            s  (Tensor): (batch, state_shape)
+        '''
+        return self.fc_layers(s)
+
+class Memory(object):
+    ''' Memory for saving transitions
+    '''
+
+    def __init__(self, memory_size, batch_size):
+        ''' Initialize
+        Args:
+            memory_size (int): the size of the memroy buffer
+        '''
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.memory = []
+
+    def save(self, state, action, reward, next_state, legal_actions, done):
+        ''' Save transition into memory
+
+        Args:
+            state (numpy.array): the current state
+            action (int): the performed action ID
+            reward (float): the reward received
+            next_state (numpy.array): the next state after performing the action
+            legal_actions (list): the legal actions of the next state
+            done (boolean): whether the episode is finished
+        '''
+        if len(self.memory) == self.memory_size:
+            self.memory.pop(0)
+        transition = Transition(state, action, reward, next_state, legal_actions, done)
+        self.memory.append(transition)
+
+    def sample(self):
+        ''' Sample a minibatch from the replay memory
+
+        Returns:
+            state_batch (list): a batch of states
+            action_batch (list): a batch of actions
+            reward_batch (list): a batch of rewards
+            next_state_batch (list): a batch of states
+            done_batch (list): a batch of dones
+        '''
+        samples = random.sample(self.memory, self.batch_size)
+        return map(np.array, zip(*samples))
